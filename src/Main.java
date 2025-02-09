882d44094dd14e5e533fff2fcf22193974076928
jainks-1 <jainks@mail.uc.edu>
2025-02-09 18:21:53 -0500

    Code finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+// Kaden Jain
+// Computer Programming 1 - Spring 2025
+/* A program that takes an integer (java int) and
+uses the modulo function to indicate if the number
+is odd or even. Since we can't do input in java yet,
+just hard code the value for the input as a variable instantiation... */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // declare variables
+        int theNumber = 4;
+
+        if (theNumber %2 == 0) {
+            System.out.println("The number is even!");
+        }
+        else {
+            System.out.println("The number is odd!");
+        }
+
     }
 }
